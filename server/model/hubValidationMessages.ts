e6533c02aea33e2efa0874bfcc1dda15d5ea9f11
yasin <yasin.mustafa@gmail.com>
2023-08-16 11:24:09 +0100

    ADJUST1-90 Add unit tests

---
+++ server/model/hubValidationMessages.ts
@@ -0,0 +1,9 @@
+const hubValidationMessages = {
+  RADA_NO_ADAS_EXIST: {
+    type: 'RESTORATION_OF_ADDITIONAL_DAYS_AWARDED',
+    action: 'VALIDATION',
+    text: 'There are currently no ADAs (Additional days awarded) recorded. You must record the ADAs before applying any RADAs.',
+  },
+}
+
+export default hubValidationMessages
