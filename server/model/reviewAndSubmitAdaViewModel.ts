908fe8a94a1b592cb777ae0b922525803cae23c1
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-10-17 13:50:34 +0100

    ADJUST1-204 quashed check your answers page.

--- server/model/reviewAndSubmitAdaViewModel.ts
+++ server/model/reviewAndSubmitAdaViewModel.ts
@@ -7,6 +7,7 @@ export default class ReviewAndSubmitAdaViewModel {
     public prisonerDetail: PrisonApiPrisoner,
     public adjustments: Adjustment[],
     public existingAdjustments: Adjustment[],
+    public quashedAdjustments: Adjustment[],
   ) {}
 
   public displayBanner(): boolean {
@@ -19,23 +20,52 @@ export default class ReviewAndSubmitAdaViewModel {
     return false
   }
 
-  public table() {
-    return {
-      caption: 'ADA details',
-      head: [{ text: 'Date charge proved' }, { text: 'Charge number(s)' }, { text: 'Days', format: 'numeric' }],
-      rows: [
-        ...this.adjustments.map(it => {
-          return [
-            { text: dayjs(it.fromDate).format('D MMM YYYY') },
-            { html: it.additionalDaysAwarded.adjudicationId.join('<br/>') },
-            { text: it.days, format: 'numeric' },
-          ]
-        }),
-        [
-          { text: 'Total ADAs', colspan: 2 },
-          { text: this.adjustments.map(a => a.days).reduce((sum, current) => sum + current, 0), format: 'numeric' },
+  public createTable() {
+    if (this.adjustments.length) {
+      return {
+        caption: 'ADA details',
+        head: [{ text: 'Date charge proved' }, { text: 'Charge number(s)' }, { text: 'Days', format: 'numeric' }],
+        rows: [
+          ...this.adjustments.map(it => {
+            return [
+              { text: dayjs(it.fromDate).format('D MMM YYYY') },
+              { html: it.additionalDaysAwarded.adjudicationId.join('<br/>') },
+              { text: it.days, format: 'numeric' },
+            ]
+          }),
+          [
+            { text: 'Total ADAs', colspan: 2 },
+            { text: this.adjustments.map(a => a.days).reduce((sum, current) => sum + current, 0), format: 'numeric' },
+          ],
         ],
-      ],
+      }
     }
+    return null
+  }
+
+  public quashedTable() {
+    if (this.quashedAdjustments.length) {
+      return {
+        caption: 'Quashed ADA details',
+        head: [{ text: 'Date charge proved' }, { text: 'Charge number(s)' }, { text: 'Days', format: 'numeric' }],
+        rows: [
+          ...this.quashedAdjustments.map(it => {
+            return [
+              { text: dayjs(it.fromDate).format('D MMM YYYY') },
+              { html: it.additionalDaysAwarded.adjudicationId.join('<br/>') },
+              { text: it.days, format: 'numeric' },
+            ]
+          }),
+          [
+            { text: 'Total Quashed ADAs', colspan: 2 },
+            {
+              text: this.quashedAdjustments.map(a => a.days).reduce((sum, current) => sum + current, 0),
+              format: 'numeric',
+            },
+          ],
+        ],
+      }
+    }
+    return null
   }
 }
