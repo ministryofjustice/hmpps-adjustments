568fd87689d2e855722b224e42064d6343fee414
ldlharper <luke.harper@digital.justice.gov.uk>
2023-12-11 10:45:35 +0000

    ADJUST1-340 fixing bugs with save remand page. (#111)

--- server/model/remandSaveModel.ts
+++ server/model/remandSaveModel.ts
@@ -13,15 +13,29 @@ export default class RemandSaveModel {
   public table() {
     return {
       head: [{ text: 'Period of remand' }, { text: 'Days spent on remand' }],
-      rows: this.rows(),
+      rows: [...this.rows(), this.totalRow()],
     }
   }
 
-  public rows() {
+  private totalRow() {
+    return [
+      {
+        text: 'Total days',
+        classes: 'govuk-table__header',
+      },
+      {
+        text: this.adjustments
+          .map(it => daysBetween(new Date(it.fromDate), new Date(it.toDate)))
+          .reduce((sum, current) => sum + current, 0),
+      },
+    ]
+  }
+
+  private rows() {
     return this.adjustments.map(it => {
       return [
         {
-          text: `${dayjs(it.fromDate).format('DD MMMM YYYY')} to ${dayjs(it.toDate).format('DD MMMM YYYY')}`,
+          text: `${dayjs(it.fromDate).format('DD MMM YYYY')} to ${dayjs(it.toDate).format('DD MMM YYYY')}`,
         },
         {
           text: daysBetween(new Date(it.fromDate), new Date(it.toDate)),
