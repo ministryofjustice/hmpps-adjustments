efd5ef524e99dc114a43c378bff51f47cb34a97a
Luke Harper <luke.harper@digital.justice.gov.uk>
2024-01-09 11:30:48 +0000

    ADJUST1-365 Remand view updates.

--- server/model/remandViewModel.ts
+++ server/model/remandViewModel.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { Adjustment } from '../@types/adjustments/adjustmentsTypes'
 import { PrisonApiOffenderSentenceAndOffences, PrisonApiPrisoner } from '../@types/prisonApi/prisonClientTypes'
 
@@ -7,6 +8,18 @@ export default class RemandViewModel {
     public adjustments: Adjustment[],
     private sentencesAndOffences: PrisonApiOffenderSentenceAndOffences[],
   ) {}
+
+  public remandTotals() {
+    return [
+      ...this.adjustments.map(it => {
+        return [
+          { text: `From ${dayjs(it.fromDate).format('DD MMM YYYY')} to ${dayjs(it.toDate).format('DD MMM YYYY')}` },
+          { text: it.daysBetween || it.effectiveDays },
+        ]
+      }),
+      [{ text: 'Total days', classes: 'govuk-table__header' }, { text: this.totalDays() }],
+    ]
+  }
 
   public adjustmentsWithOffences() {
     return this.adjustments.map(it => {
