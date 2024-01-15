6ad3d1efffe23be1231ed07f4a32c62111c6387c
yasin <yasin.mustafa@gmail.com>
2024-01-15 10:10:45 +0000

    add active sentence data

--- server/model/taggedBailSelectCaseModel.ts
+++ server/model/taggedBailSelectCaseModel.ts
@@ -1,9 +1,16 @@
-import { PrisonApiPrisoner } from '../@types/prisonApi/prisonClientTypes'
+import { PrisonApiOffenderSentenceAndOffences, PrisonApiPrisoner } from '../@types/prisonApi/prisonClientTypes'
 
 export default class TaggedBailSelectCaseModel {
-  constructor(public prisonerDetail: PrisonApiPrisoner) {}
+  constructor(
+    public prisonerDetail: PrisonApiPrisoner,
+    private sentencesAndOffences: PrisonApiOffenderSentenceAndOffences[],
+  ) {}
 
   public backlink(): string {
     return `/${this.prisonerDetail.offenderNo}`
+  }
+
+  public activeSentences(): PrisonApiOffenderSentenceAndOffences[] {
+    return this.sentencesAndOffences.filter(it => it.sentenceStatus === 'A')
   }
 }
--- server/routes/taggedBailRoutes.ts
+++ server/routes/taggedBailRoutes.ts
@@ -17,9 +17,13 @@ export default class TaggedBailRoutes {
     const { caseloads, token } = res.locals.user
     const { nomsId } = req.params
     const prisonerDetail = await this.prisonerService.getPrisonerDetail(nomsId, caseloads, token)
+    const sentencesAndOffences = await this.prisonerService.getSentencesAndOffencesFilteredForRemand(
+      prisonerDetail.bookingId,
+      token,
+    )
 
     return res.render('pages/adjustments/tagged-bail/select-case', {
-      model: new TaggedBailSelectCaseModel(prisonerDetail),
+      model: new TaggedBailSelectCaseModel(prisonerDetail, sentencesAndOffences),
     })
   }
 }
