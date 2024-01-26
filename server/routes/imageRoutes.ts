7b8a8424ff4fcd147895178bac284a050ccc1bd6
yasin <yasin.mustafa@gmail.com>
2024-01-26 14:29:23 +0000

    Fetch prisoner image

---
+++ server/routes/imageRoutes.ts
@@ -0,0 +1,24 @@
+import { RequestHandler } from 'express'
+import path from 'path'
+import PrisonerService from '../services/prisonerService'
+
+const placeHolderImage = path.join(process.cwd(), '/assets/images/prisoner-profile-image.png')
+export default class ImageRoutes {
+  constructor(private readonly prisonerService: PrisonerService) {}
+
+  public get: RequestHandler = async (req, res): Promise<void> => {
+    const { prisonerNumber } = req.params
+
+    return this.prisonerService
+      .getPrisonerImage(res.locals.user.token, prisonerNumber)
+      .then(data => {
+        res.set('Cache-control', 'private, max-age=86400')
+        res.removeHeader('pragma')
+        res.type('image/jpeg')
+        data.pipe(res)
+      })
+      .catch(_error => {
+        res.sendFile(placeHolderImage)
+      })
+  }
+}
