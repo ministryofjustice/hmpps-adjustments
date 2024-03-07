21d1b90783aad3e029018621a966ebeaab4e9770
Pavilion Sahota <pavilion.sahota@justice.gov.uk>
2024-03-07 11:45:05 +0000

    Adjusted delete tagged bail page

--- integration_tests/pages/tagged-bail/removeTaggedBail.ts
+++ integration_tests/pages/tagged-bail/removeTaggedBail.ts
@@ -3,7 +3,7 @@ import { PageElement } from '../page'
 
 export default class RemoveTaggedBailPage extends AdjustmentsPage {
   constructor() {
-    super('Delete Tagged Bail details')
+    super('Delete Tagged Bail')
   }
 
   public submit = (): PageElement => cy.get('[data-qa=remove-button]')
