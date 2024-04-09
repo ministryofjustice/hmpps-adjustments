b295a617b4a892635ae5f381feb065ddff842fc3
Pavilion Sahota <pavilion.sahota@justice.gov.uk>
2024-04-09 16:02:37 +0100

    Using GDS padding classes over custom ones

--- server/model/taggedBailViewModel.ts
+++ server/model/taggedBailViewModel.ts
@@ -65,10 +65,10 @@ export default class TaggedBailViewModel {
   private actionCell(adjustment: Adjustment, caseReference: string, courtDescription: string) {
     return {
       html: `
-      <div class="govuk-grid-column-one-quarter govuk-!-margin-right-2 no-padding">
+      <div class="govuk-grid-column-one-quarter govuk-!-margin-right-2 govuk-!-padding-0">
         <a class="govuk-link" href="/${adjustment.person}/tagged-bail/edit/${adjustment.id}" data-qa="edit-${adjustment.id}">Edit<span class="govuk-visually-hidden"> tagged bail for case ${caseReference} at ${courtDescription}</span></a><br />
       </div>
-      <div class="govuk-grid-column-one-half no-padding">
+      <div class="govuk-grid-column-one-half govuk-!-padding-0">
         <a class="govuk-link" href="/${adjustment.person}/tagged-bail/remove/${adjustment.id}" data-qa="delete-${adjustment.id}">Delete<span class="govuk-visually-hidden"> tagged bail for case ${caseReference} at ${courtDescription}</span></a><br />
       </div>
     `,
