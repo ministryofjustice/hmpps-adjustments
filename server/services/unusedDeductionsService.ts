d06c651b58b9926c3e42110191cec1ab87da7381
Pavilion Sahota <pavilion.sahota@justice.gov.uk>
2024-06-13 15:01:56 +0100

    Added new cases for the unused deductions banner

--- server/services/unusedDeductionsService.ts
+++ server/services/unusedDeductionsService.ts
@@ -3,7 +3,14 @@ import { delay } from '../utils/utils'
 import AdjustmentsService from './adjustmentsService'
 import CalculateReleaseDatesService from './calculateReleaseDatesService'
 
-export type UnusedDeductionMessageType = 'NOMIS_ADJUSTMENT' | 'VALIDATION' | 'UNSUPPORTED' | 'UNKNOWN' | 'NONE'
+export type UnusedDeductionMessageType =
+  | 'NOMIS_ADJUSTMENT'
+  | 'VALIDATION'
+  | 'UNSUPPORTED'
+  | 'UNKNOWN'
+  | 'CRDS_CALL_FAILED'
+  | 'CRDS_CALL_FAILED_NONE'
+  | 'NONE'
 
 export default class UnusedDeductionsService {
   private maxTries = 6 // 3 seconds max wait
@@ -82,7 +89,11 @@ export default class UnusedDeductionsService {
 
       return ['UNKNOWN', adjustments]
     } catch {
-      return ['UNKNOWN', adjustments]
+      if (this.getTotalUnusedRemand(adjustments) > 0) {
+        return ['CRDS_CALL_FAILED', adjustments]
+      }
+
+      return ['CRDS_CALL_FAILED_NONE', adjustments]
     }
   }
 
