307c93d1b9c6f1237dd23cdcb7c0a2ffab1552b2
ldlharper <luke.harper@digital.justice.gov.uk>
2024-06-21 11:22:06 +0100

    ADJST-688 Review PADAs cosmetic updates. (#361)

--- server/routes/hubRoutes.test.ts
+++ server/routes/hubRoutes.test.ts
@@ -317,7 +317,7 @@ describe('GET /:nomsId', () => {
       .get(`/${NOMS_ID}`)
       .expect('Content-Type', /html/)
       .expect(res => {
-        expect(res.text).toContain('<a href="/ABC123/additional-days/review-prospective">Review PADAs</a>')
+        expect(res.text).toContain('<a href="/ABC123/additional-days/review-prospective">Review unapplied PADAs</a>')
       })
   })
 
