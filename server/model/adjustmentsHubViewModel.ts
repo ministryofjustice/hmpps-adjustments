ce2805ec2650887c3066714dce307665cf8b5e09
Pavilion Sahota <pavilion.sahota@justice.gov.uk>
2024-06-28 15:57:02 +0100

    Updated the show unused deductions check

--- server/model/adjustmentsHubViewModel.ts
+++ server/model/adjustmentsHubViewModel.ts
@@ -134,35 +134,11 @@ export default class AdjustmentsHubViewModel {
   }
 
   public getUnused(adjustmentType: AdjustmentType): number {
-    if (this.unusedDeductionMessage === 'NONE') {
+    if (this.unusedDeductionMessage === 'NONE' || this.unusedDeductionMessage === 'UNSUPPORTED') {
       const adjustments = this.adjustments.filter(it => it.adjustmentType === adjustmentType.value)
       const total = adjustments.map(a => a.days).reduce((sum, current) => sum + current, 0)
       const effective = adjustments.map(a => a.effectiveDays).reduce((sum, current) => sum + current, 0)
       return total - effective
-    }
-
-    if (this.unusedDeductionMessage === 'UNSUPPORTED') {
-      const unusedDeductionAdjustmentDays =
-        this.adjustments.filter(it => it.source !== 'NOMIS').find(it => it.adjustmentType === 'UNUSED_DEDUCTIONS')
-          ?.effectiveDays || 0
-      const remandAdjustmentDays =
-        this.adjustments
-          .filter(it => it.adjustmentType === 'REMAND')
-          ?.map(it => it.days)
-          .reduce((acc, cur) => {
-            return acc + cur
-          }, 0) || 0
-      if (adjustmentType.value === 'REMAND') {
-        return unusedDeductionAdjustmentDays > remandAdjustmentDays
-          ? remandAdjustmentDays
-          : unusedDeductionAdjustmentDays
-      }
-
-      if (adjustmentType.value === 'TAGGED_BAIL') {
-        return unusedDeductionAdjustmentDays > remandAdjustmentDays
-          ? unusedDeductionAdjustmentDays - remandAdjustmentDays
-          : 0
-      }
     }
 
     return 0
