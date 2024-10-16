f63c9e5fe3180052cddcdbde0b7ef5b96cc624a9
dave-virden <151018159+dave-virden@users.noreply.github.com>
2024-10-16 15:24:14 +0100

    ADJST-859 Tidy up following review

--- server/model/specialRemissionType.ts
+++ server/model/specialRemissionType.ts
@@ -3,14 +3,14 @@ const specialRemissionType = [
     value: 'MERITORIOUS_CONDUCT',
     text: 'Meritorious (excellent) conduct',
     hint: {
-      text: 'The person has been rewarded with automatic release. or consideration for release by the Parole Board at an earlier date. If the person is on licence, their SLED is being brought forward',
+      text: 'The person has been rewarded with automatic release. or consideration for release by the Parole Board at an earlier date. If the person is on licence, their SLED is being brought forward.',
     },
   },
   {
     value: 'RELEASE_DATE_CALCULATED_TOO_EARLY',
     text: 'Release date calculated too early',
     hint: {
-      text: 'The balance of the sentence to be served to the correct release date has been cancelled out. The person will be released on an earlier date. Release in error',
+      text: 'The balance of the sentence to be served to the correct release date has been cancelled out. The person will be released on an earlier date.',
     },
   },
   {
