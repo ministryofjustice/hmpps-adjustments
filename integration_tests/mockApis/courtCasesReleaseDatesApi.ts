2c25bf69f0f491df80ec92f5145621d2627bac43
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-01-21 09:27:54 +0000

    Correcting width of things to do.

--- integration_tests/mockApis/courtCasesReleaseDatesApi.ts
+++ integration_tests/mockApis/courtCasesReleaseDatesApi.ts
@@ -18,6 +18,7 @@ export default {
               text: 'Overview',
               thingsToDo: {
                 count: 0,
+                things: [],
               },
             },
             adjustments: {
@@ -25,6 +26,7 @@ export default {
               text: 'Adjustments',
               thingsToDo: {
                 count: 0,
+                things: [],
               },
             },
             releaseDates: {
@@ -32,6 +34,7 @@ export default {
               text: 'Release dates and calculations',
               thingsToDo: {
                 count: 0,
+                things: [],
               },
             },
           },
