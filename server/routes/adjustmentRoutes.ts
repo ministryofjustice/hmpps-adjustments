41d9b7fa9718737964ee68e83f824592907850f2
Jade Alvares <jade.alvares@justice.gov.uk>
2025-03-25 09:52:45 +0000

    removed second table const, integration tests should pass

--- server/routes/adjustmentRoutes.ts
+++ server/routes/adjustmentRoutes.ts
@@ -300,13 +300,8 @@ export default class AdjustmentRoutes {
       adjustmentType.value === 'REMAND' && roles.includes('REMAND_IDENTIFIER')
         ? await this.identifyRemandPeriodsService.getRemandDecision(nomsId, username)
         : null
-    const secondTable =
-      adjustmentType.value === 'UNLAWFULLY_AT_LARGE'
-        ? await this.identifyRemandPeriodsService.getRemandDecision(nomsId, username)
-        : null
     return res.render('pages/adjustments/view', {
       model: new ViewModel(adjustments, adjustmentType, remandDecision, roles),
-      secondTable,
     })
   }
 
