28dec46c707aa1de795a4e8f0b918cb0e1edacfc
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2025-05-12 11:27:10 +0100

    ADJST-1002 Correcting JS imports and Adding logging statement for support user

--- server/middleware/supportUserReadonlyMiddleware.ts
+++ server/middleware/supportUserReadonlyMiddleware.ts
@@ -1,8 +1,10 @@
 import type { RequestHandler } from 'express'
+import logger from '../../logger'
 
 export default function supportUserReadonlyMiddleware(): RequestHandler {
   return async (req, res, next) => {
     if (res.locals.user.isSupportUser) {
+      logger.info('The signed in user is a support user, redirecting to authError')
       return res.redirect('/authError')
     }
     return next()
