104693fec0bc0ff96f33fd71ac81d5832cf6030c
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-07-11 09:39:19 +0100

    Only block support users from edit on production

--- server/middleware/supportUserReadonlyMiddleware.ts
+++ server/middleware/supportUserReadonlyMiddleware.ts
@@ -1,9 +1,10 @@
 import type { RequestHandler } from 'express'
 import logger from '../../logger'
+import config from '../config'
 
 export default function supportUserReadonlyMiddleware(): RequestHandler {
   return async (req, res, next) => {
-    if (res.locals.user.isSupportUser) {
+    if (res.locals.user.isSupportUser && config.blockSupportUsersFromEdit) {
       logger.info('The signed in user is a support user, redirecting to authError')
       return res.redirect('/authError')
     }
