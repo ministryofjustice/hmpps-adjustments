2cc7f41fe2f9856391d9ee175ccb01b91111f24d
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2025-12-15 10:50:26 +0000

    Correct linting

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -7,6 +7,6 @@ export default configureAllowedScripts({
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/esbuild@0.25.12': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
-    "node_modules/unrs-resolver@1.7.13": 'FORBID'
+    'node_modules/unrs-resolver@1.7.13': 'FORBID',
   },
 })
